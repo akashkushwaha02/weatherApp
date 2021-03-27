9124f91b810ef869e1b41b50c8006f389399dc92
akash <akash.kushwaha02@gmail.com>
2021-03-27 18:54:40 +0530

    added models and model form

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    city = models.CharField(null=False,max_length=40)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.city
